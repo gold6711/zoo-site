d6d010825c805980730a9fe6d48308fb4a599625
Igor <gold6711@gmail.com>
2017-12-26 03:09:33 +0300

    html css coding- second part

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     pug = require('gulp-pug'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
     browserSync = require('browser-sync');
 
 gulp.task('sass', function(){ // Создаем таск "sass"
@@ -17,6 +21,23 @@ gulp.task('pug', function(){ // Создаем таск "pug"
         .pipe(browserSync.reload({stream: true})) // Обновляем
 });
 
+gulp.task('scripts', function() {
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('css-libs',['sass'], function() {
+    return gulp.src('app/css/libs.css')
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'));
+});
+
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browser Sync
         server: { // Определяем параметры сервера
@@ -26,7 +47,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
     });
 });
 
-gulp.task('watch', ['browser-sync', 'sass', 'pug'], function() {
+gulp.task('watch', ['browser-sync', 'css-libs', 'pug', 'scripts'], function() {
     gulp.watch('app/pug/**/*.pug', ['pug']);
     gulp.watch('app/sass/**/*.sass', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
